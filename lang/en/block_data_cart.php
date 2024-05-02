dcb79ebea55615f746ba6ffc735a74390904564b
Valery Fremaux <valery.fremaux@gmail.com>
2024-05-02 11:11:59 +0200

    Automated sync from SVN

---
+++ lang/en/block_data_cart.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_html', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   block_data_cart
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['configtitle'] = 'Data Cart block title';
+$string['configdocumentproducerbinding'] = 'Document Producer Binding';
+$string['data_cart:addinstance'] = 'Add a new Data Cart block';
+$string['newblock'] = '(new Data cart block)';
+$string['pluginname'] = 'Data Cart';
+$string['export'] = 'Export';
+$string['anonexport'] = 'Export anonymized';
+$string['reset'] = 'Reset';
+$string['configanonymize'] = 'Anonymize';
+$string['configanonymize_help'] = 'If this option is enabled, firtname, lastname and email info should be mangled in output document';
+$string['configlistfields'] = 'List fields';
+$string['configlistfields_help'] = 'Give the list of field names that makes (in order) the list item name';
+$string['configsensiblefields'] = 'Sensible fields';
+$string['configsensiblefields_help'] = 'Give the list of field names that need to be mangled';
+$string['norecords'] = 'No records in cart';
+$string['privacy:metadata:block'] = 'The Data Cart block stores all of its data within the block subsystem.';
+
+$string['configdocumentproducerbinding_help'] = 'Document producer is a a course module instance compatible with data cart to produce an
+output document for exporting. At the moment, only APL PdCertificate Module implements the expected API';
